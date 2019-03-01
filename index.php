e43f501feee019b28bcc478230878e479f7b2fa7
rosamaria.ramirez <rosamaria.ramirez@salud-juntaex.es>
2019-03-01 12:49:54 +0100

    Cambio el titulo - pruebas ROSA 2

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Pruebas ROSA</title>
+        <title>Pruebas ROSA 2</title>
     </head>
     <body>
         <?php
