a7d071918bdbe1e26b5ee4f4d7fb4859ff30508c
manuel.gonzalezco <manuel.gonzalezco@salud-juntaex.es>
2019-03-01 12:33:57 +0100

    cambios de prueba manuel

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        // put your code here -- Manuel 01-03-2019
         ?>
     </body>
 </html>
